da49001369d038ed77fab5c881c556a7a3df24be
llumi <juliaoncrack@gmail.com>
2024-08-14 16:51:27 -0400

    Added styling.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import requests
-import urllib.request
+from colorama import Fore, init
 from bs4 import BeautifulSoup
 
+init(autoreset=True)
     
 def download_image(url, download_name):
     response = requests.get(url)
@@ -10,24 +11,33 @@ def download_image(url, download_name):
         file.write(response.content)
     
 
-url = input("Enter Soundcloud URL: ")
+url = input(Fore.CYAN + "Enter Soundcloud URL\n> " + Fore.WHITE)
+
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
 if response.status_code == 200:
+    # Find's the "img" tag in html
     image = soup.find('img')  
+    
+    # If the tag is found, it grabs the source and image name (alt)
+    # Then proceeds to download it
     if image:
         image_source = image['src']
         image_name = image['alt'] + ".jpg"
-        print("Image source: ", image_source, "\nImage name: ", image_name)
+        
+        # Printing out image details
+        print(Fore.YELLOW + "Image source: ", Fore.WHITE + image_source, Fore.YELLOW + "\nImage name: ", Fore.WHITE + image_name)
+        
+        save_type = input(Fore.CYAN + "Save image as... (.jpg, .png)\n> " + Fore.WHITE)
         print("Downloading...")
         try:
             download_image(image_source, image_name)
-            print('Download successful!')
+            print(Fore.GREEN + 'Download successful!')
         except Exception as e:
-            print(f"Error: {e}")
+            print(Fore.RED + f"Error: {e}")
 
     else:
-        print("Image not found.")
+        print(Fore.RED + "Image not found.")
 else:
-    print("Failed to retrieve page.")
+    print(Fore.RED + "Failed to retrieve page.")
